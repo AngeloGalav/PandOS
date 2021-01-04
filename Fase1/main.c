07dd6635094bb3a51d276f7559c2d287890e2076
Void <peze95@hotmail.it>
2021-01-04 22:16:07 +0100

    latest main

--- Fase1/main.c
+++ Fase1/main.c
@@ -1,6 +1,3 @@
-//#include "pandos_const.h"
-//#include "pandos_types.h"
-
 #include "pcb_test.h"
 
 #define MAXPROC 20
@@ -41,39 +38,32 @@ void initPcbs()
 {
     //setting up the cursor
     pcb_t* tmp = &pcbFree_h;
-
-    //p_sentinel->head = pcbFree_h;
-
+    p_sentinel->head = pcbFree_h;
     pcbFree_h = &pcbFree_table[0];
     pcbFree_h->val = 0;
     pcbFree_h = pcbFree_h->p_next;
-
     for (int i = 1; i<MAXPROC-1; i=i+1)
     {
         pcbFree_h = &pcbFree_table[i]; //malloc
         pcbFree_h->val = i;
         printf("val: %d, memaddr: %d\n",  pcbFree_h->val,  pcbFree_h);
-        //tmp->p_prev = &pcbFree_table[i-1];
+        tmp->p_prev = &pcbFree_table[i-1];
         pcbFree_h = pcbFree_h->p_next;
     }
-    //p_sentinel->size = 0; //questo ci da problemi
+    p_sentinel->size = 0; //questo ci da problemi
     pcbFree_h = &pcbFree_table[MAXPROC];
     pcbFree_h->val = MAXPROC;
-   // tmp->p_prev = &pcbFree_table[MAXPROC-1];
+    tmp->p_prev = &pcbFree_table[MAXPROC-1];
     pcbFree_h->p_next = tmp;
     pcbFree_h = pcbFree_h->p_next;
-
-  // printList(pcbFree_h, MAXPROC);
-
-   // pcbFree_h->p_prev = &tmp;
-
-  // pcbFree_h = tmp;
+    printList(pcbFree_h, MAXPROC);
+    pcbFree_h->p_prev = &tmp;
+    pcbFree_h = tmp;
 }
 */
 
 void initPcbs()
 {
-
     pcb_t* hd = &pcbFree_h;
 
     hd->val = 0;
@@ -113,16 +103,6 @@ pcb_t *allocPcb()
         pcb_t* temp = pcbFree_h;
         pcb_t* tail = pcbFree_h->p_prev;
         p_sentinel->head = pcbFree_h->p_next;
-
-        /*
-        temp->p_next = NULL;
-        temp->p_prev = NULL;
-        temp->p_prnt = NULL;
-        temp->p_child = NULL;
-        temp->p_next_sib = NULL;
-        temp->p_prev_sib = NULL;
-        temp->p_s = NULL;
-        */
 
         initList(temp);
 
@@ -164,50 +144,48 @@ void initList(pcb_t* node)
     node->p_prev_sib = NULL;
 }
 
-void tail_insert(pcb_t* pcbFree_h, pcb_t* elem)
+void tail_insert(pcb_t* list, pcb_t* elem)
 {
-    if(pcbFree_h != NULL)
+    if(list != NULL)
     {
-        if(pcbFree_h->p_next == NULL)
+        if(list->p_next == NULL)
         {
-            pcbFree_h->p_next == elem;
-            pcbFree_h->p_prev == elem;
-            elem->p_next = pcbFree_h;
-            elem->p_prev = pcbFree_h;
+            list->p_next == elem;
+            list->p_prev == elem;
+            elem->p_next = list;
+            elem->p_prev = list;
         }
         else
         {
-            pcb_t* tail = pcbFree_h->p_prev;
+            pcb_t* tail = list->p_prev;
             tail->p_next = elem;
             elem->p_prev = tail;
-            elem->p_next = pcbFree_h;
-            pcbFree_h->p_prev = elem;
+            elem->p_next = list;
+            list->p_prev = elem;
         }
     }
 
 }
 
-void head_insert(pcb_t* pcbFree_h,pcb_t* elem)
+void head_insert(pcb_t* list,pcb_t* elem)
 {
-    if(pcbFree_h != NULL)
+    if(list != NULL)
     {
-        if(pcbFree_h->p_next == NULL)
+        if(list->p_next == NULL)
         {
-            pcbFree_h->p_next == elem;
-            pcbFree_h->p_prev == elem;
-            elem->p_next = pcbFree_h;
-            elem->p_prev = pcbFree_h;
+            list->p_next == elem;
+            list->p_prev == elem;
+            elem->p_next = list;
+            elem->p_prev = list;
         }
         else
         {
-            elem->p_next = pcbFree_h->p_next;
-            elem->p_prev = pcbFree_h;
-            pcbFree_h->p_next = elem;
+            elem->p_next = list->p_next;
+            elem->p_prev = list;
+            list->p_next = elem;
             elem->p_next->p_prev = elem;
         }
-
     }
-
 }
 
 int emptyProcQ(pcb_t *tp)
@@ -219,13 +197,88 @@ int emptyProcQ(pcb_t *tp)
 
 void insertProcQ(pcb_t** tp, pcb_t* p)
 {
-    *tp = &pcbFree_h; // ora è tail
-    *tp->p_prev->p_next = &p;
-    *tp->p_prev = p;
+    //printf("queue: %d p: %d\n", *tp, p);
 
-    
+    printf("\n\npasso 0\n");
+    printf("p val:\n");
+    printf("prev: %d | actual: %d | next: %d\n", p->p_prev, p, p->p_next);
+    printf("tp val:\n");
+    printf("prev: %d | actual: %d | next: %d\n", (*tp)->p_prev, *tp , (*tp)->p_next);
+
+    pcb_t* temp = (*tp)->p_prev;
+
+    printf("\n\npasso 1\n");
+    printf("temp val:\n");
+    printf("prev: %d | actual: %d | next: %d\n", temp->p_prev, temp , (temp)->p_next);
+    printf("p val:\n");
+    printf("prev: %d | actual: %d | next: %d\n", p->p_prev, p, p->p_next);
+    printf("tp val:\n");
+    printf("prev: %d | actual: %d | next: %d\n", (*tp)->p_prev, *tp , (*tp)->p_next);
+
+    (*tp)->p_prev = p;
+
+    printf("\n\npasso 2\n");
+    printf("temp val:\n");
+    printf("prev: %d | actual: %d | next: %d\n", temp->p_prev, temp , (temp)->p_next);
+    printf("p val:\n");
+    printf("prev: %d | actual: %d | next: %d\n", p->p_prev, p, p->p_next);
+    printf("tp val:\n");
+    printf("prev: %d | actual: %d | next: %d\n", (*tp)->p_prev, *tp , (*tp)->p_next);
+
+    p->p_prev = temp;
+
+    printf("\n\npasso 3\n");
+    printf("temp val:\n");
+    printf("prev: %d | actual: %d | next: %d\n", temp->p_prev, temp , (temp)->p_next);
+    printf("p val:\n");
+    printf("prev: %d | actual: %d | next: %d\n", p->p_prev, p, p->p_next);
+    printf("tp val:\n");
+    printf("prev: %d | actual: %d | next: %d\n", (*tp)->p_prev, *tp , (*tp)->p_next);
+
+    p->p_next = *tp;
+
+    printf("\n\npasso 4\n");
+    printf("temp val:\n");
+    printf("prev: %d | actual: %d | next: %d\n", temp->p_prev, temp , (temp)->p_next);
+    printf("p val:\n");
+    printf("prev: %d | actual: %d | next: %d\n", p->p_prev, p, p->p_next);
+    printf("tp val:\n");
+    printf("prev: %d | actual: %d | next: %d\n", (*tp)->p_prev, *tp , (*tp)->p_next);
+
+    (temp)->p_next = p;
+
+    printf("\n\npasso 5\n");
+    printf("temp val:\n");
+    printf("prev: %d | actual: %d | next: %d\n", temp->p_prev, temp , (temp)->p_next);
+    printf("p val:\n");
+    printf("prev: %d | actual: %d | next: %d\n", p->p_prev, p, p->p_next);
+    printf("tp val:\n");
+    printf("prev: %d | actual: %d | next: %d\n", (*tp)->p_prev, *tp , (*tp)->p_next);
 }
 
+
+void tail_insert_ini(pcb_t** tp, pcb_t* elem)
+{
+    if(*tp != NULL)
+    {
+        if((*tp)->p_next == NULL)
+        {
+            (*tp)->p_next == elem;
+            (*tp)->p_prev == elem;
+            elem->p_next = (*tp);
+            elem->p_prev = (*tp);
+        }
+        else
+        {
+            pcb_t* tail = (*tp)->p_prev;
+            tail->p_next = elem;
+            elem->p_prev = tail;
+            elem->p_next = (*tp);
+            (*tp)->p_prev = elem;
+        }
+    }
+
+}
 
 int main()
 {
@@ -234,14 +287,26 @@ int main()
 
     arrayTest = fillList(arrayTest, MemoryAlloc);
 
-    printf("\nListPrint");
-    printList(pcbFree_h, MAXPROC+3);
-    printf("REVERSED\n", pcbFree_h, &pcbFree_h);
-    reversePrintList(pcbFree_h, MAXPROC+3);
+    //printf("\nListPrint");
+   // printList(&pcbFree_h, MAXPROC+3);
+
+    pcb_t tests;
+    pcb_t* test;
+
+    test = &tests;
+
+    test->val = 69;
+
+    printf("addr di pcbFree_h: %d addr di test: %d\n", pcbFree_h, test);
+
+    //insertProcQ(&pcbFree_h, test);
+
+    //insertProcQ2(&pcbFree_h, test);
+
+    //printf("\nREVERSED\n", pcbFree_h, &pcbFree_h);
+    //reversePrintList(&pcbFree_h, MAXPROC+3);
+
+    //printList(pcbFree_h, MAXPROC+3);
+
     return 0;
 }
-
-
-
-
-
