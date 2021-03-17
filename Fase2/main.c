e1e277019fc82a9abc697b2cf63c1fd0b5cff2c0
AngeloGalav <tekken342@gmail.com>
2021-03-17 14:21:35 +0100

    AGGIUNTO HIDDEN E ALCUNE COSE

--- Fase2/main.c
+++ Fase2/main.c
@@ -5,38 +5,43 @@
 #include "umps3/umps/bios_defs.h"
 #include "umps3/umps/cp0.h"
 #include "umps3/umps/regdef.h"
-#include <umps3/umps/libumps.h>
+#include "umps3/umps/libumps.h"
 #include "../Libraries/pcb.h"
 #include "../Libraries/asl.h"
 #include "../Libraries/libraries.h"
 
 /* Define the 100000 milliseconds */
-#define TIMERVALUE  (PSECOND / * ((unsigned int*) TIMESCALEADDR))
+#define TIMERVALUE  (PSECOND / * ((unsigned int*) TIMESCALEADDR)) /// TODO: vedere questo 
 #define DEVICES_NUMBER 17
 /* Include the test function */
 extern void test();
 
 /* Number of started, but not yet terminated processes. */
-unsigned int processCount = 0;
+HIDDEN unsigned int processCount = 0;
 
 /* Number of started, but not terminated 
 processes that in are the “blocked” state 
 due to an I/O or timer request */
-unsigned int softBlockCount = 0;
+HIDDEN unsigned int softBlockCount = 0;
 
 /* Tail pointer to a queue of pcbs that are in the “ready” state. */
-pcb_PTR readyQueue;
+HIDDEN pcb_PTR readyQueue;
 
-/* Pointer to the current active process in "ready" state */ 
-pcb_PTR currentProcess;
+/* Pointer to the current pcb that is in running state */ 
+HIDDEN pcb_PTR currentProcess = NULL;
 
 //TO-DO capire come inizializzare i semaphores devices
+/* Int Array for semaphores*/
+HIDDEN int semaphores[2*(DEVICES_NUMBER + 1)]; // we need two semaphore for each device + 1
 
 /* Inizialize pass-up-vector with the addressess needed */
-passupvector_t* passupvector ;
+HIDDEN passupvector_t* passupvector;
 
-/* Int Array for semaphores*/
-int semaphores[DEVICES_NUMBER];
+/*Placeholder function for TLB-Refill*/
+void uTLB_RefillHandler ();
+
+/*placeholder function for exception handling*/
+void fooBar();
 
 int main()
 {
@@ -46,13 +51,18 @@ int main()
     initASL();
 
     /* Fill up pass-up-vector*/
-    passupvector->tlb_refill_handler = (memaddr) uTLB_RefillHandler() ; //Codice già dato nel p2test.c
+    passupvector->tlb_refill_handler = (memaddr) uTLB_RefillHandler; //Codice già dato nel p2test.c
     passupvector->tlb_refill_stackPtr = (memaddr) 0x20001000;
-    passupvector->exception_stackPtr = (memaddr) 0x20001000 ;
-    passupvector->exception_handler = (memaddr) fooBar(); //fooBar is for exception
+    passupvector->exception_stackPtr = (memaddr) 0x20001000;
+    passupvector->exception_handler = (memaddr) fooBar; // exception handling function callback
 
+    
+    /// TODO: Qui dobbiamo mettere un for che inizializza i semafori dei device appena sappiamo come
+    /// questi sono definiti...
+    
+    
     if(!getSTATUS()) // check the kernel mode ?????
-        LDIT(TIMERVALUE);
+        LDIT(TIMERVALUE); /* TODO: initCPU? */
 
     /* Start the process initialization */
     pcb_PTR proc = allocPcb() ;
@@ -74,4 +84,14 @@ int main()
 
     // 00001000000000000000000000001001 == 134217737 passare un numero che mette i bit dello status register come vogliamo?
     proc->p_s.status = setSTATUS(134217737);
+}
+
+
+void uTLB_RefillHandler () {
+		
+	setENTRYHI(0x80000000);
+	setENTRYLO(0x00000000);
+	TLBWR();	
+	
+	LDST ((state_t *) 0x0FFFF000);
 }
