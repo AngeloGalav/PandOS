76c28c677f02691899c9f2770f8f770377b475e6
AngeloGalav <tekken342@gmail.com>
2021-03-29 19:44:21 +0200

    push

--- Fase2/devinterrupt_handler.c
+++ Fase2/devinterrupt_handler.c
@@ -4,5 +4,14 @@ void InterruptPendingChecker(unsigned int cause_reg)
 {
     //(memaddr) INTDEVBITMAP;
     unsigned int ip_reg = BitExtractor(cause_reg, 0xFF00, 8);
+
+    int k = 2;
+    for (int i = 1; i < 8; i++)
+    {
+        if (ip_reg & k) 
+            InterruptLineDeviceCheck(i);
+        k *= 2;
+    }
+    
         
 }
