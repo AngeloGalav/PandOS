6d93032ece5c4f0042877edcc116753e28cc0678
AngeloGalav <tekken342@gmail.com>
2021-04-05 16:05:02 +0200

    updated debugger

--- Fase2/debugger.c
+++ Fase2/debugger.c
@@ -1,20 +1,6 @@
 #include "../include/debugger.h"
 
 void bp(void){};
-void bp_INIZIO(void){};
-void bp_bhochestrano(void){};
-void bp_primoif(){};
-void bp_faPUfine(){}
-void bp_syscall_(void){};
-void bp_interval(){};
-void bp_godo(){};
 void bp_wait(){};
 void bp_correct(){};
-void memcpy_start(){};
-void memcpy_end(){};
-void bp_p_inizio(){};
-void zombie_process_excp(){};
-void bp_TLBPU(){};
-void bp_entra_insBlock(){};
-void bp_wait2(){};
 void bp_ignore(){};
--- include/debugger.h
+++ include/debugger.h
@@ -2,27 +2,8 @@
 #include "pandos_const.h"
 #include "pandos_types.h"
 
-/* Breakpoint function created for debugging the system */
+/* Breakpoint functions created for debugging the system */
 void bp(void);
-
-/* Extra breakpoint */
-void bp_INIZIO(void);
-
-/* Scheduler error breakpoint */
-void bp_bhochestrano(void);
-
-void bp_primoif();
-void bp_p_inizio();
-void bp_faPUfine();
 void bp_wait();
-void bp_interval();
-void bp_godo();
-void bp_syscall_(void);
 void bp_correct();
-void memcpy_start();
-void memcpy_end();
-void zombie_process_excp();
-void bp_TLBPU();
-void bp_entra_insBlock();
-void bp_wait2();
 void bp_ignore();
