f6f4154f1a1fa642157343b2a29e393adcd1debc
Void <peze95@hotmail.it>
2021-05-23 14:05:30 +0200

    j

--- Fase3/sysSupport.c
+++ Fase3/sysSupport.c
@@ -73,18 +73,14 @@ void Get_Tod_SYS10(unsigned int *regv0) // we need to check if the pointers are 
 void  Write_To_Printer_SYS11(support_t* sPtr)
 {
     // Printer device associated with the U-proc 
-    // IntLno 6
-    // DevNo = current Asid 
     devreg_t* devReg = DEV_REG_ADDR(6, sPtr->sup_asid);
     // Is an error to write to a printer device from an address outside of the requesting 
     // U-proc’s logical address space, but how can we check it ????????
-    // Lo scopriremo in un'altra puntata di MISTERO
-    // devo mettere da a1 dentro DATA0 per la lunghezza di a2
-    //Ogni carattere è un byte quindi ogni carattere è un ciclo in cui faccio :
-    // metto il carattere da a1 in DATA0, setto lo status e scrivo il comando per scrivere
-    //immagino che in a2 ci sia il numero di caratteri quindi di byte per il mio ciclo
-    // inoltre controllo che se ho a2 < 0 || a2 > 128 dò errore quindi SYS9
+    
+    // P for the sempaphore ?
+
     unsigned int strlength = (unsigned int) sPtr->sup_exceptState->reg_a2 ;
+
     if((strlength >= 0 ) && (strlength <= MAXSTRLENG))
     {
         char *s = sPtr->sup_exceptState->reg_a1;
@@ -95,9 +91,11 @@ void  Write_To_Printer_SYS11(support_t* sPtr)
             devReg->dtp.command = PRINTCHR;
 
             s++;
-            //bisogna controllare che sia andato a buon fine ad ogni iterazione e quindi in caso no riprovare ?
+            //check status value each cycle ?
         }
     }
     else
         Terminate_SYS9();
+    
+    //V for the semaphore ?
 }
