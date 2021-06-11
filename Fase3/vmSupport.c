a2f410e93678442d4f445ae85bf138017cd63eae
Void <peze95@hotmail.it>
2021-06-11 21:26:07 +0200

    okbuf

--- Fase3/vmSupport.c
+++ Fase3/vmSupport.c
@@ -13,6 +13,14 @@ void initSwapStructs()
    
     swap_semaphore = 1;
 
-    
+    //initialize the swap table as empty in the beginning
+    // every ASID should be set as -1
+    // the start of the first page is calculated by overestimate the OS code size
+    for (int i = 0 ; i < POOLSIZE ; i++)
+    {
+        swap_table[i].sw_asid = NOPROC ;
+        swap_table[i].sw_pageNo = FLASHPOOLSTART + (PAGESIZE * i ); // every page is 4096 bytes after the other
+        swap_table[i].sw_pte = NULL;
+    }
 
 }
